8201e28862f160bf40bb6f5ce02d9255ccc52382
ManassesGamabunta <manasses.java@gmail.com>
2020-08-23 19:01:28 -0300

    commit inicial

---
+++ build/classes-generated/moduloAtualizaProjeto/Bundle.java
@@ -0,0 +1,27 @@
+package moduloAtualizaProjeto;
+/** Localizable strings for {@link moduloAtualizaProjeto}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>tela</i>
+     * @see telaTopComponent
+     */
+    static String CTL_telaAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_telaAction");
+    }
+    /**
+     * @return <i>tela Window</i>
+     * @see telaTopComponent
+     */
+    static String CTL_telaTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_telaTopComponent");
+    }
+    /**
+     * @return <i>This is a tela window</i>
+     * @see telaTopComponent
+     */
+    static String HINT_telaTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_telaTopComponent");
+    }
+    private Bundle() {}
+}
